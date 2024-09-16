2ced5c805445e315f720809e067d012a452da690
NyanLinHtoo <nyanlinhtoo.ygn@gmail.com>
2024-09-16 23:19:45 +0700

    add layout of RMP

--- src/component/MusicPlayer.tsx
+++ src/component/MusicPlayer.tsx
@@ -1,5 +1,23 @@
 const MusicPlayer = () => {
-  return <div className="w-24 h-[320px] bg-slate-600">MusicPlayer</div>;
+  return (
+    <div className="text-center">
+      <div className="w-[260px] h-[320px] bg-slate-600">
+        <p>MusicPlayer</p>
+        <p>Song name</p>
+        <p>Artist</p>
+        <img src="" alt="" />
+        <p>00:00</p>
+        <p>03:22</p>
+        <p>Progress Bar</p>
+        <p>
+          button group
+          <p>back</p>
+          <p>play</p>
+          <p>next</p>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default MusicPlayer;
