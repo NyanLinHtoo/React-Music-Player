067e86172d858c88fcd10fb889f06681c97e65a8
NyanLinHtoo <nyanlinhtoo.ygn@gmail.com>
2024-09-24 23:28:34 +0700

    Progressing Music player

--- src/component/MusicPlayer.tsx
+++ src/component/MusicPlayer.tsx
@@ -1,21 +1,54 @@
+import { useState } from "react";
+import { Card } from "antd";
+
+import useSound from "use-sound";
+import PlayButton from "./PlayButton";
+
 const MusicPlayer = () => {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [play, { stop }] = useSound(soundUrl, {
+    onend: () => setIsPlaying(false),
+  });
   return (
-    <div className="text-center">
-      <div className="w-[260px] h-[320px] bg-slate-600">
-        <p>MusicPlayer</p>
-        <p>Song name</p>
-        <p>Artist</p>
-        <img src="" alt="" />
-        <p>00:00</p>
-        <p>03:22</p>
-        <p>Progress Bar</p>
-        <p>
+    <div className="flex justify-center items-center h-screen bg-gray-800">
+      <Card className="bg-slate-600 font-roboto" style={{ width: 300 }}>
+        <div>
+          <p>MusicPlayer</p>
+          <p>Song name</p>
+          <p>Artist</p>
+        </div>
+        <div>
+          <img src="" alt="" />
+        </div>
+        <div>
+          <p>00:00</p>
+          <p>03:22</p>
+        </div>
+        <div>
+          <p>Progress Bar</p>
+        </div>
+        <div>
           button group
           <p>back</p>
-          <p>play</p>
+          <PlayButton
+            className={styles.btn}
+            active={isPlaying}
+            size={60}
+            iconColor="hsl(0deg 0% 100%)"
+            idleBackgroundColor="hsl(210deg 15% 6%)"
+            activeBackgroundColor="hsl(240deg 95% 62%)"
+            play={() => {
+              play();
+              setIsPlaying(true);
+            }}
+            stop={() => {
+              stop();
+              setIsPlaying(false);
+            }}
+          />
           <p>next</p>
-        </p>
-      </div>
+        </div>
+      </Card>
     </div>
   );
 };
